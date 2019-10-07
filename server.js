2ae3b426698b04f053c9ec1b3faaceb0f47fe51a
Sunny Singh <singhsunny0712@gmail.com>
2019-10-07 09:56:43 +0530

    new updated files

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ var server=http.createServer(function(req,res){
     res.end();
 });
 
-server.listen(3000,function(){
+var port =process.env.PORT || 3000;
+server.listen(port,function(){
     console.log("Server has started  on port 3000");
 })
 
